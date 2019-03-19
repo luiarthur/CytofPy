d5463d300cb467d2fc272be853b50303aaef9ff2
luiarthur <luiarthur@gmail.com>
2019-03-19 14:33:26 -0700

    added pretty-print function for distributions

--- sims/cb/util.py
+++ sims/cb/util.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+from torch.distributions import Normal, Gamma, Beta, Dirichlet
+from torch.distributions.log_normal import LogNormal
 
 def get_one_hot(targets, nb_classes, use_np=False):
     if use_np:
@@ -9,3 +11,30 @@ def get_one_hot(targets, nb_classes, use_np=False):
         # use torch
         res = torch.eye(nb_classes)[targets.clone().reshape(-1)]
         return res.reshape(list(targets.shape) + [nb_classes])
+
+def pretty_dist(D, warn=False):
+    if isinstance(D, Normal):
+        mean = D.loc
+        sd = D.scale
+        out = 'Normal(mean: {}, sd: {})'.format(mean, sd)
+    elif isinstance(D, Gamma):
+        conc = D.concentration
+        rate = D.rate
+        out = 'Gamma(shape: {}, rate: {})'.format(conc, rate)
+    elif isinstance(D, LogNormal):
+        loc = D.loc
+        scale = D.scale
+        out = 'LogNormal(loc: {}, scale: {})'.format(loc, scale)
+    elif isinstance(D, Beta):
+        a = D.concentration0
+        b = D.concentration1
+        out = 'Beta(a: {}, b: {})'.format(a, b)
+    elif isinstance(D, Dirichlet):
+        conc = D.concentration
+        out = 'Dir(conc: {})'.format(conc)
+    else:
+        if warn:
+            print('WARNING: `pretty_dist` NOT IMPLEMENTED for this distribution!')
+        out = str(D)
+
+    return out
