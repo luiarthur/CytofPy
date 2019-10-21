009a7b549df0fc04ddfd69bfd23ac5fb58c1d2d5
Arthur Lui <luiarthur@gmail.com>
2019-10-21 12:05:32 -0700

    added some imputation docs and plots

--- sims/cb/possible_imputation_strategy.R
+++ sims/cb/possible_imputation_strategy.R
@@ -7,6 +7,8 @@ M = is.na(Y)
 y1 = Y[1:41474, ]
 m1 = is.na(y1)
 
+# TODO: use a neural net
+
 # Impute missing values in a column
 impute = function(Yi, Mi, k) {
   # get indices for observed Y in column k
@@ -35,8 +37,15 @@ impute = function(Yi, Mi, k) {
 # Impute all missing values in a matrix
 impute_all = function(Yi, Mi, max_iter=30, tol=1e-3) {
   X = y1
-  X[m1] = rnorm(sum(m1), -3, sd=.1)
+  # X[m1] = rnorm(sum(m1), -3, sd=.1)
   K = NCOL(X)
+  # Initialize
+  for (k in 1:K) {
+    num_missing = sum(m1[, k])
+    neg_obs_idx = which(X[, k] < 0)
+    X[m1[, k], k] = sample(X[neg_obs_idx, k], replace=TRUE, size=num_missing)
+  }
+
   diff_X = c()
  
   for (i in 1:max_iter) {
@@ -60,3 +69,18 @@ out = impute_all(y1, m1, max_iter=10)
 y1_new = out$X
 plot(out$diff_X, type='o')
 hist(y1_new[,4])
+
+is_good_row = function(x) all(abs(x) < 6)
+y1_new_good_rows = apply(y1_new, 1, is_good_row)
+y1_new_good = y1_new[y1_new_good_rows, ]
+m1_good = m1[y1_new_good_rows, ]
+
+plotme = function(j) {
+  par(mfrow=c(1, 3))
+  hist(y1_new_good[m1_good[, j], j], main="new missing only")
+  hist(y1_new_good[, j], main="new")
+  hist(y1[, j], main="old")
+  par(mfrow=c(1, 1))
+}
+
+plotme(9)
